b5f8587684dde502a7bf077624b06735ca3040db
Erika <erikacardon@gmail.com>
2021-01-14 17:14:02 -0800

    Week Two Update

--- js/main.js
+++ js/main.js
@@ -1,11 +1,28 @@
 function showAllWeeks() {
 
+    // const links = [{
+    //     label: "Week1 Notes",
+    //     url: "weekOne/index.html"
+    // }
+    //     {
+    //     label: "Week2 Notes",
+    //     url: "weekTwo/index.html"
+    // }]
+
+    // const links = [
+    //     [label: "Week1 Notes", url: "weekOne/index.html"],
+    //     [label: "Week2 Notes", url: "weekTwo/index.html"]
+    // ];
+
     const links = [{
-        label: "Week1 Notes",
-        url: "weekOne/index.html";
-        label: "Week2 Notes",
-        url: "weekTwo/index.html";
-    }]
+            label: "Week1 Notes",
+            url: "weekOne/index.html"
+        },
+        {
+            label: "Week2 Notes",
+            url: "weekTwo/index.html"
+        }]
+
     if (links != null) {
         // var allWeeks = JSON.parse(links)
         var weeksDisplayer = document.getElementById("all_weeks_display")
@@ -13,7 +30,7 @@ function showAllWeeks() {
         var numberOfWeeks = links.length
         for (var i = 0; i < numberOfWeeks; i++) {
             var aWeek = links[i]
-            weeksDisplayer.innerHTML += "<a href=" + aWeek["url"] + ">" + aWeek["label"] + "</a>"
+            weeksDisplayer.innerHTML += "<a href=" + aWeek["url"] + ">" + aWeek["label"] + "</a><br>"
         }
     }
 }
